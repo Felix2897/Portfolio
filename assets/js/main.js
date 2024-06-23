140ea74b6b255231968243f078754fb4b0430459
Felix2897 <andrea.feliziani97@gmail.com>
2024-06-23 12:02:00 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,139 @@
+/* ----- NAVIGATION BAR FUNCTION ----- */
+function myMenuFunction(){
+  var menuBtn = document.getElementById("myNavMenu");
+
+  if(menuBtn.className === "nav-menu"){
+    menuBtn.className += " responsive";
+  } else {
+    menuBtn.className = "nav-menu";
+  }
+}
+
+/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
+window.onscroll = function() {headerShadow()};
+
+function headerShadow() {
+  const navHeader =document.getElementById("header");
+
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
+
+    navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+    navHeader.style.height = "70px";
+    navHeader.style.lineHeight = "70px";
+
+  } else {
+
+    navHeader.style.boxShadow = "none";
+    navHeader.style.height = "90px";
+    navHeader.style.lineHeight = "90px";
+
+  }
+}
+
+
+/* ----- TYPING EFFECT ----- */
+var typingEffect = new Typed(".typedText",{
+  strings : ["UI/UX Designer"," Front-End Developer"],
+  loop : true,
+  typeSpeed : 100,
+  backSpeed : 80,
+  backDelay : 2000
+})
+
+
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+const sr = ScrollReveal({
+      origin: 'top',
+      distance: '80px',
+      duration: 2000,
+      reset: true
+})
+
+/* -- HOME -- */
+sr.reveal('.featured-text-card',{})
+sr.reveal('.featured-name',{delay: 100})
+sr.reveal('.featured-text-info',{delay: 200})
+sr.reveal('.featured-text-btn',{delay: 200})
+sr.reveal('.social_icons',{delay: 200})
+sr.reveal('.featured-image',{delay: 300})
+
+
+/* -- PROJECT BOX -- */
+sr.reveal('.project-box',{interval: 200})
+
+
+
+
+/* -- HEADINGS -- */
+sr.reveal('.top-header',{})
+
+/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
+
+/* -- ABOUT INFO & CONTACT INFO -- */
+const srLeft = ScrollReveal({
+origin: 'left',
+distance: '80px',
+duration: 2000,
+reset: true
+})
+
+srLeft.reveal('.about-info',{delay: 100})
+srLeft.reveal('.contact-info',{delay: 100})
+
+
+
+
+
+/* ----- CHANGE ACTIVE LINK ----- */
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+const scrollY = window.scrollY;
+
+sections.forEach(current =>{
+  const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 50,
+    sectionId = current.getAttribute('id')
+
+  if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+
+  }  else {
+
+    document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+
+  }
+})
+}
+
+window.addEventListener('scroll', scrollActive)
+
+
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+const progress = document.getElementById("progress");
+const steps = document.getElementsByClassName("circle");
+
+let activeIndex = 0;
+const setActive = (toggle, index) => {
+  progress.style.width = `${(toggle ? index : index - 1) * (100 / 3)}%`;
+  steps[index].classList.toggle("active", toggle);
+};
+next.addEventListener("click", () => {
+  activeIndex += 1;
+  setActive(true, activeIndex);
+  prev.disabled = false;
+  if (activeIndex === steps.length - 1) {
+    next.disabled = true;
+  }
+});
+prev.addEventListener("click", () => {
+  next.disabled = false;
+  setActive(false, activeIndex);
+  activeIndex -= 1;
+  if (activeIndex === 0) {
+    prev.disabled = true;
+  }
+});
